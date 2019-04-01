a896418f51a569dc24841c160195413def154a17
Ronald Bruce <ronald.bruce@wsu.edu>
2019-03-31 20:54:07 -0700

    adding comments about get user route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,11 +7,26 @@ router.get('/addUser', function(req, res) {
   res.send("Index response");
 });
 
+/*
+example get url: http://localhost:5000/api/users/getUser?user=User1
+this assumes:
+1. the mongoDB database is named "mydb"
+2. the collection is called "users"
+3. url passes a parameter called "user" which is the name of the user
+
+example response:
+[
+    {
+        "_id": "5ca1780cf4e003274804047d",
+        "name": "User1",
+        "todo": "[]"
+    }
+]
+*/
 router.get('/getUser', function(req, res) {
   mongoClient.connect(url, function(err, db) {
     if (err) throw err;
     const dbo = db.db('mydb');
-    console.log(req.query);
     dbo.collection("users").find({name: req.query.user}).toArray(function(err, res1) {
         if(err) {
             throw err;
