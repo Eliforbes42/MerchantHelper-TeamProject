fbfc7db2954ef365edb8693a30add73db10591ad
Ronald Bruce <ronald.bruce@wsu.edu>
2019-03-31 20:26:26 -0700

    get user request working with hardcoded value

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
+let mongoClient = require('mongodb').MongoClient;
+const url = `mongodb://localhost:27017/mydb`;
 
 router.get('/addUser', function(req, res) {
   res.send("Index response");
 });
 
 router.get('/getUser', function(req, res) {
-    res.send("Index response");
-  });
-
+  mongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    const dbo = db.db('mydb');
+    dbo.collection("users").find({name: "User1"}).toArray(function(err, res1) {
+        if(err) {
+            throw err;
+        } else{
+          res.send(res1);
+        }
+    });
+    
+});
+});
 module.exports = router;
