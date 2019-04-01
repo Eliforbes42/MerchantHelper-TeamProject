803448122119d8dc37469a1c36b93362f2490a64
Ronald Bruce <ronald.bruce@wsu.edu>
2019-03-31 20:27:40 -0700

    adding db.js file back to outer folder

---
+++ backend/db.js
@@ -0,0 +1,39 @@
+let mongoClient = require('mongodb').MongoClient;
+const url = `mongodb://localhost:27017/mydb`;
+
+module.exports = {
+    insert: (data, collection)=>{
+        mongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            const dbo = db.db('mydb');
+            dbo.collection(collection).insertMany([data],function(err, res){
+                if (err) throw err;
+            });
+        });
+    },
+    update: (id, data, collection)=>{
+        mongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            const dbo = db.db('mydb');
+            const pk = {id: id};
+            const updates = {$set: data};
+            dbo.collection(collection).updateOne(pk, updates,function(err, res){
+                if (err) throw err;
+            });
+        });
+    },
+    findUser: (name, collection)=>{
+        mongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            const dbo = db.db('mydb');
+            dbo.collection(collection).find({name: name}).toArray(function(err, res1) {
+                if(err) {
+                    throw err;
+                } else{
+                  return res1;
+                }
+            });
+            
+        });
+    }
+} //export
